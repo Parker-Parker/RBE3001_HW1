b76119a0b8004924604f135522b1a5fa543a05fd
Parker-Parker <35411451+Parker-Parker@users.noreply.github.com>
2018-01-17 13:41:54 -0500

    Added body to DummyServer::event

--- src/coms/DummyServer.cpp
+++ src/coms/DummyServer.cpp
@@ -4,4 +4,7 @@ void DummyServer::event(float * buffer){
 
  // DO something
  // set values back to Buffer
+   uint8_t * buff = (uint8_t *) buffer; //Cast buffer address to array of unsigned integers, so it is easy to fill with zeroes 
+   for(int i = 0; i < 64;i++) //Fill returning packet with 0s
+      buff[i]=0x00;
 }
